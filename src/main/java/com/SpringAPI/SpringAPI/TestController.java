0f3028ebb5fbd3110c6942fa4b90f03fc35a5a98
Pratiksha Kute <pratikute1603@gmail.com>
2022-10-09 15:42:11 +0530

    initial version

--- src/main/java/com/SpringAPI/SpringAPI/TestController.java
+++ src/main/java/com/SpringAPI/SpringAPI/TestController.java
@@ -1,4 +1,18 @@
 package com.SpringAPI.SpringAPI;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test()
+    {
+        int a=121;
+        int b=20;
+        return "this is for testing"+(a+b);
+    }
+
 }
