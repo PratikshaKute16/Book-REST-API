5cfdde5083249c7420fa3b9eabe61ef95c1d7a19
Pratiksha Kute <pratikute1603@gmail.com>
2022-10-12 18:15:52 +0530

    initial version

--- src/main/java/com/SpringAPI/SpringAPI/dao/BookRepo.java
+++ src/main/java/com/SpringAPI/SpringAPI/dao/BookRepo.java
@@ -3,6 +3,7 @@ package com.SpringAPI.SpringAPI.dao;
 import com.SpringAPI.SpringAPI.Entities.Book;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface BookRepo extends CrudRepository<Book,Integer> {
     public Book findById(int id);
 }
